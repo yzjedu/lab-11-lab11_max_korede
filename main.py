61214f971d9acd210d8cf055c73b4aa10835b37a
maxrice <mxrice@loyola.edu>
2024-11-21 13:28:55 -0500

    .

--- main.py
+++ main.py
@@ -10,3 +10,13 @@ def write_file_to_dict(filename):
     return dict
 
 print(write_file_to_dict("morsecode.txt"))
+
+def file_to_letters(dict, filename):
+    table = []
+    fd = open(filename, "w")
+    for line in fd:
+        row = line.split()
+        table.append(row)
+    for word in line:
+        fd.write(word)
+    return table
