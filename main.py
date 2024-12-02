05cc35b3340590aa4cd7eef097b2d22a16708aa0
Korede Oni <oaoni@loyola.edu>
2024-12-02 13:21:27 -0500

    Added function to help organize the list

--- main.py
+++ main.py
@@ -1,22 +1,45 @@
 def write_file_to_dict(filename):
-    dict = {}
+    morse_dict = {}
     input_file = open(filename, "r")
     for line in input_file:
         items = line.split('  ')
         key = items[1].strip()
         value = items[0].split()
-        dict[key] = value
+        morse_dict[key] = value
     input_file.close()
-    return dict
+    return morse_dict
 
-print(write_file_to_dict("morsecode.txt"))
+lem = write_file_to_dict("morsecode.txt")
 
-def file_to_letters(dict, filename):
+def read_code_to_table(filename):
     table = []
-    fd = open(filename, "w")
-    for line in fd:
+    input_file = open(filename, "r")
+    data = input_file.readlines()
+    for line in data:
         row = line.split()
         table.append(row)
-    for word in line:
-        fd.write(word)
     return table
+
+def convert_table_to_morse(dictionary, table):
+    for i in range(len(table)):
+        for j in range(len(table[i])):
+            table[i][j] = dictionary[table[i][j]]
+    return table
+
+def reorganize_table(table):
+    new_table = []
+    temp_list = []
+    for line in table:
+        for item in line:
+            temp_list.append(item[0])
+        new_table.append(temp_list)
+        temp_list = []
+    return new_table
+
+domo = read_code_to_table("morse1.txt")
+
+masa = convert_table_to_morse(lem, domo)
+
+print(read_code_to_table("morse1.txt"))
+print(masa)
+print(reorganize_table(masa))
